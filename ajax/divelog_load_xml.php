cb56ce41c8c84aa7dc36b13751554fac9d43b7ba
Mark Brettin <mark@jackrabbitsystems.com>
2013-12-29 12:26:24 -0700

    Code cleanup

--- ajax/divelog_load_xml.php
+++ ajax/divelog_load_xml.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once 'config.php';
+    require_once('config.php');
 
     $email = (isset($_REQUEST['email']) && $_REQUEST['email'] != '') ? $_REQUEST['email'] : '';
     if($email == '') { print "ERROR: email is empty"; exit(); }
